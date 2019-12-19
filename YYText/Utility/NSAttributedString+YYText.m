84510290e6b6804d472bafc1ca059e72bf6566a4
liujixin <liuxiaoxinxin@users.noreply.github.com>
2019-12-19 17:56:02 +0800

    Update NSAttributedString+YYText.m
    修复iOS13 上设置yy_setBaselineOffset不生效的问题。

--- YYText/Utility/NSAttributedString+YYText.m
+++ YYText/Utility/NSAttributedString+YYText.m
@@ -1056,7 +1056,11 @@ return style. _attr_;
 
 - (void)yy_setBaselineOffset:(NSNumber *)baselineOffset range:(NSRange)range {
     if (kSystemVersion >= 7) {
-        [self yy_setAttribute:NSBaselineOffsetAttributeName value:baselineOffset range:range];
+        if (@available(iOS 11.0, *)) {
+            [self yy_setAttribute:(NSString *)kCTBaselineOffsetAttributeName value:baselineOffset range:range];
+        } else {
+            [self yy_setAttribute:NSBaselineOffsetAttributeName value:baselineOffset range:range];
+        }
     }
 }
 
